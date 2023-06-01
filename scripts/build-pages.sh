5689838ae653979c95fa49f552a256ca1277e871
Pryke, Benjamin <benjamin.pryke@jpmorgan.com>
2023-06-01 17:34:10 +0100

    fix: use appropriate PUBLIC_URL for local vs cloudflare pages envs

--- scripts/build-pages.sh
+++ scripts/build-pages.sh
@@ -1,5 +1,11 @@
-rm -fr dist
-./scripts/scaffold.sh
+# Clean up
+yarn clean
+
+# Set up
+MICROFRONTENDS_ROOT="/microfrontends"
+. ./scripts/scaffold.sh
+
+# Create registry
 REACT_APP_REGISTY_URL="/microfrontends/registry.json" yarn modular build host
 mkdir -p dist/pages/microfrontends
 echo '[' >> dist/pages/microfrontends/registry.json
@@ -7,6 +13,8 @@ echo '{ "name": "card-view", "root": "/microfrontends/card-view", "manifest": "/
 echo '{ "name": "grid-view", "root": "/microfrontends/grid-view", "manifest": "/microfrontends/grid-view/package.json" },' >> dist/pages/microfrontends/registry.json
 echo '{ "name": "simple-app", "root": "/microfrontends/simple-app", "manifest": "/microfrontends/simple-app/package.json" }' >> dist/pages/microfrontends/registry.json
 echo ']' >> dist/pages/microfrontends/registry.json
+
+# Create dir structure
 cp -r dist/host/* dist/pages
 mv dist/card-view dist/pages/microfrontends/card-view
 mv dist/grid-view dist/pages/microfrontends/grid-view
--- scripts/scaffold.sh
+++ scripts/scaffold.sh
@@ -2,4 +2,4 @@ yarn modular add card-view --template=card-view
 yarn modular add grid-view --template=grid-view
 yarn modular add simple-app --template=iframe-app
 yarn modular build card-view grid-view
-PUBLIC_URL="/simple-app" yarn modular build simple-app
+PUBLIC_URL="$MICROFRONTENDS_ROOT/simple-app" yarn modular build simple-app
